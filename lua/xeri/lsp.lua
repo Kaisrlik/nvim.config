7d3912e1c59e59658e1d162c0fc86377d66d509c
Jan Kaisrlik <ja.kaisrlik@gmail.com>
2024-05-03 15:25:33 +0200

    lsp: enable ltex-ls for emails as well

--- lua/xeri/lsp.lua
+++ lua/xeri/lsp.lua
@@ -129,7 +129,7 @@ end
 
 lspconfig.ltex.setup {
 	on_attach = on_attach,
-	filetypes = { "markdown", "text", "tex" },
+	filetypes = { "markdown", "text", "tex", "mail" },
 	cmd = { "ltex-ls" },
 	flags = { debounce_text_changes = 300 },
 	settings = {
